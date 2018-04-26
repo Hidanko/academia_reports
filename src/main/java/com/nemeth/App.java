59b55e17be23d17ea464b9754f736ef6a734cb5b
Gustavo Nemeth <hidan_nemeth@hotmail.com>
2018-04-26 13:41:59 -0300

    Bugs fixed

--- src/main/java/com/nemeth/App.java
+++ src/main/java/com/nemeth/App.java
@@ -13,22 +13,18 @@ import net.sf.dynamicreports.report.builder.column.TextColumnBuilder;
 import net.sf.dynamicreports.report.datasource.DRDataSource;
 import net.sf.dynamicreports.report.exception.DRException;
 
-
 public class App {
 	public static void main(String[] args) throws DRException {
-		TextColumnBuilder<String> colunaData = col.column("Data", "data", type.stringType());
-		TextColumnBuilder<String> colunaPorcentagem = col.column("Data", "data", type.stringType());
+		TextColumnBuilder<String> colunaData = col.column("Dia", "dia", type.stringType());
+		TextColumnBuilder<Integer> colunaPorcentagem = col.column("Porcentagem", "porcentagem", type.integerType());
+
 
 		LineChartBuilder lineChart = cht.lineChart()
 
-				.setShowPercentages(true)
-
-				.setShowValues(true)
+				.setShowPercentages(true).setShowValues(true)
 				.setDataSource(createDatasource())
-				.setCategory(colunaPorcentagem)
-		// .setSeriesOrderBy(seriesOrderBy)
-		// .series(cht.serie(colunaData))
-		;
+				.setCategory(colunaData)
+				.seriesOrderBy("id").series(cht.serie(colunaPorcentagem));
 
 		report().setTemplate(Templates.reportTemplate)
 				.addTitle(Templates.createTitleComponent("RELATÓRIO \n " + "NOME_DO_ALUNO", "ACADEMIA DO BUGHI",
@@ -41,23 +37,25 @@ public class App {
 				.addTitle(cmp.verticalList(cmp.text(""),
 						cmp.text("Relatório do dia " + "DATA_INICIAL" + " até o dia " + "DATA_FINAL" + ".")
 								.setStyle(Templates.bold12CenteredStyle)))
-				
+
 				.addTitle(lineChart)
-				
+
 				.addPageFooter(Templates.footerComponent,
 						cmp.text("TEXTO_MOTIVACIONAL").setStyle(Templates.rodapeDireita))
+				.setDataSource(createDatasource())
 				.show();
+				
 	}
 
 	private static DRDataSource createDatasource() {
 		DRDataSource lista = new DRDataSource("id", "dia", "porcentagem");
 
-		lista.add(1, 02 / 02, 80);
-		lista.add(2, 03 / 02, 88);
-		lista.add(3, 04 / 02, 100);
-		lista.add(4, 06 / 02, 100);
-		lista.add(5, 07 / 02, 70);
-		lista.add(6, 10 / 02, 90);
+		lista.add(1, "02 / 02", 80);
+		lista.add(2, "03 / 02", 88);
+		lista.add(3, "04 / 02", 100);
+		lista.add(4, "06 / 02", 100);
+		lista.add(5, "07 / 02", 70);
+		lista.add(6, "10 / 02", 90);
 
 		return lista;
 	}
